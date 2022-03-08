8256213b6bdd2fb3a4ba14fdaea1e5336de8948d
Gerardo Schekaiban <ggschekaiban@hotmail.com>
2022-03-07 20:46:10 -0600

    Create mailer and message to send it

--- config/constants.php
+++ config/constants.php
@@ -1,5 +1,8 @@
 <?php
 
+define ('EMAIL', 'gerardogschekaiban@gmail.com');
+define ('PASSWORD', 'qsnv51596');
+
 define('DB_HOST', 'localhost');
 define('DB_USER', 'root');
 define('DB_PASSWORD', '');
--- controllers/emailController.php
+++ controllers/emailController.php
@@ -1,5 +1,46 @@
 <?php
 
+require_once 'vendor/autoload.php';
+require_once 'config/constants.php';
+
+// Create the Transport
+$transport = (new Swift_SmtpTransport('smtp.gmail.com', 465, 'ssl'))
+  ->setUsername(EMAIL)
+  ->setPassword(PASSWORD)
+;
+
+// Create the Mailer using your created Transport
+$mailer = new Swift_Mailer($transport);
+
+
+
 function sendVerificationEmail($userEmail, $token){
-    
+
+  global $mailer;
+  
+  $body = '<!DOCTYPE html>
+  <html lang="en">
+  <head>
+      <meta charset="UTF-8">
+      <title>Verify email</title>
+  </head>
+  <body>
+      <div class="warapper">
+          <p>
+              Thank you for signing up on our website. Please click on the link below to verify your email.
+          </p>
+          <a href="http://localhost/userAuthPhp/index.php?token=' .$token . '">Verify your email adress</a>
+      </div>
+  </body>
+  </html>';
+
+  // Create a message
+  $message = (new Swift_Message('Verify your email address'))
+  ->setFrom([EMAIL])
+  ->setTo([$userEmail])
+  ->setBody($body)
+  ;
+  
+  // Send the message
+  $result = $mailer->send($message);
 }
